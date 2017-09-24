4507d5e353eed69e510d457c1235a7fa8e034248
sajen87 <bolsik@gmail.com>
2017-09-24 11:15:40 +0200

    Couple solutions for codeWars ex

---
+++ codeWarsExcercises.js
@@ -0,0 +1,27 @@
+
+
+function highAndLow(numbers){
+    return console.log(numbers.split());
+
+}
+
+var x = highAndLow("1 2 3 78");
+
+
+
+
+
+function getCount(str) {
+    var vowelsCount = 0;
+
+vowelsCount = str.split()
+
+    function vowels(age){
+    return
+    }
+
+return console.log(vowelsCount)
+    return vowelsCount;
+}
+
+getCount("abracadabra");
