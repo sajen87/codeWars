9c39219f72822ba3d5216a707939f5250ab9cad5
sajen87 <bolsik@gmail.com>
2017-10-21 21:41:47 +0200

    multiples 3 & 5 function

--- Multiples3.5.js
+++ Multiples3.5.js
@@ -1,10 +1,43 @@
-var numbers = [1, 3, 5, 7, 10, 14, 30]
-var newNumbers;
+const numbers = [1, 3, 5, 7, 10, 14, 30]
 
+const threeFive = numbers.filter(function(n){
 
-function solution(newNumbers) {
-  var newNumbers = numbers.filter(function(i){
-    return (i % 3 === 0 || i % 5 === 0)
+    if(n % 3 === 0 || n % 5 === 0){
+      return n;
+    }
+    else {
+      console.log("zla liczba")
+    }
   })
 
-}
+
+const sum = threeFive.reduce(function (n, sum) {
+
+    return n + sum
+
+
+})
+
+console.log(sum)
+
+
+
+// function solution(...number) {
+//
+//   const threeFive = number.filter(function (n) {
+//     if (n % 3 === 0 || n % 5 === 0) {
+//       return n;
+//     }
+//
+//   })
+//
+//   const sum = threeFive.reduce(function (n, sum) {
+//
+//     return n + sum
+//
+//
+//   })
+// }
+//
+// solution(1, 3, 5, 7, 10, 14, 30)
+//
