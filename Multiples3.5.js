db225f463bf958ce9e0dd2068df5db24c700b3a7
sajen87 <bolsik@gmail.com>
2017-10-22 12:50:30 +0200

    refactored Multiples 3/5 - kata 6

--- Multiples3.5.js
+++ Multiples3.5.js
@@ -1,43 +1,18 @@
-const numbers = [1, 3, 5, 7, 10, 14, 30]
+function solution(...number) {
 
-const threeFive = numbers.filter(function(n){
+  let threeFive = number.filter(function (n) {
 
-    if(n % 3 === 0 || n % 5 === 0){
+    if (n % 3 === 0 || n % 5 === 0) {
+
       return n;
+
     }
-    else {
-      console.log("zla liczba")
-    }
+
+  }).reduce(function (m, sum) {
+    return m + sum;
   })
 
+  return threeFive
+}
 
-const sum = threeFive.reduce(function (n, sum) {
-
-    return n + sum
-
-
-})
-
-console.log(sum)
-
-
-
-// function solution(...number) {
-//
-//   const threeFive = number.filter(function (n) {
-//     if (n % 3 === 0 || n % 5 === 0) {
-//       return n;
-//     }
-//
-//   })
-//
-//   const sum = threeFive.reduce(function (n, sum) {
-//
-//     return n + sum
-//
-//
-//   })
-// }
-//
-// solution(1, 3, 5, 7, 10, 14, 30)
-//
+console.log(solution(1,2,3,4,5,6,7,8,9))
